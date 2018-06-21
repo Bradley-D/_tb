246efb04f5b6616348e1e4aab61460220cfcfaa8
Bradley Davis <Bradley-D@users.noreply.github.com>
2018-06-21 17:32:28 +1000

    [ ISSUE #1 ] add theme support for gallery

--- includes/tb-wc-functions.php
+++ includes/tb-wc-functions.php
@@ -16,6 +16,11 @@
 // 	define( 'WOOCOMMERCE_USE_CSS', false );
 // }
 
+// Add theme support for the product gallery from WC2.7
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox' );
+add_theme_support( 'wc-product-gallery-slider' );
+
 // Unhook WooCommerce wrappers
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
